1a90b72460218ff8ee27e6405eb16339596b1aba
Sachin Kumar Jaiswal <jaiswalsachings@gmail.com>
2025-08-25 21:35:05 +0530

    Update dummydata.js

--- client/src/lib/dummydata.js
+++ client/src/lib/dummydata.js
@@ -12,7 +12,7 @@ export const listData = [
   },
   {
     id: 2,
-    title: "An Awesome Apartment Near the Park! Almost too good to be true!",
+    title: "An Awesome Apartment Near the Park.....! Almost too good to be true!",
     images: ["https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"],
     bedroom: 3,
     bathroom: 2,
